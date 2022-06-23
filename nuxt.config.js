aacbcd980a9c1e115ec56d3522e77bdc912161e9
Thomas Vandenbussche <thomas.vandenbussche@outlook.com>
2022-06-23 15:36:45 +0200

    attempt to deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,7 +44,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: `http://${process.env.API_HOST}:${process.env.API_PORT}`,
+    baseURL: `http://localhost:3001`,
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
