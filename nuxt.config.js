7a897f1dfe99df056668ec01d93d60bb2dbf40ac
Thomas Vandenbussche <thomas.vandenbussche@outlook.com>
2022-06-26 15:31:32 +0200

    connecting to heroku api

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,9 +44,11 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: `https://yummying-api.herokuapp.com/`,
+    proxy: true,
   },
-
+  proxy: {
+    '/api/': { target: `https://yummying-api.herokuapp.com/`, pathRewrite: {'^/api/': ''} }
+  },
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
 }
